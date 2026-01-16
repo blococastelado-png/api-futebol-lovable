28cf68789cd662e55304b6068d173c0500278ab7
blococastelado-png <blococastelado@gmail.com>
2026-01-16 11:21:31 -0300

    Update games.js

--- api/games.js
+++ api/games.js
@@ -1,8 +1,7 @@
 export default async function handler(req, res) {
   const response = await fetch("https://v3.football.api-sports.io/fixtures?date=2026-01-16", {
     headers: {
-      "x-apisports-key": "a5c2a4b76757e2163325b67332787f3d
-"
+      "x-apisports-key": "a5c2a4b76757e2163325b67332787f3d"
     }
   });
 
